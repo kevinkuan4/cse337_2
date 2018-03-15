45e961e64d7b4bb74e9afad83b57d914a27d2cd3
Kevin Kuan <kevinkuan4@gmail.com>
2018-03-15 18:20:07 -0400

    Adding q1_p1.pl

---
+++ Kevin_Kuan_109645104/q1_p1.pl
@@ -0,0 +1,10 @@
+# Kevin Kuan
+# 109645104
+# CSE 3337 HW 2
+
+use strict;
+use warnings;
+use diagnostics;
+
+my $apple_variety;
+$apple_variety= "Red Delicious";
---
+++ q1_p1.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+
+# Kevin Kuan
+# 109645104
+# CSE 3337 HW 2
+
+# use strict;
+# use warnings;
+# use diagnostics;
+use strict;
+use warnings;
+
+print "Hello, world\n";
+
+my $apple_variety = "Red Delicious";
