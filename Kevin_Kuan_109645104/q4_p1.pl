ae2da67c9acfe1cbb4638a84d95be9e2b353fb3f
Kevin <kevinkuan4@gmail.com>
2018-03-17 23:09:56 -0400

    q4_p1 start

---
+++ Kevin_Kuan_109645104/q4_p1.pl
@@ -0,0 +1,82 @@
+# Kevin Kuan
+# CSE 337
+# Homework 2
+
+print "Enter file name: ";
+my $filename = <STDIN>; 
+chomp $filename;
+
+my $valid_input = 0;
+my @temp_storage;
+my $line_count = 0;
+# file exists
+if (-e $filename){
+	print "$filename already exists. ";
+
+	# backup directory exists
+	if (-e "./backup"){
+		print "Checking backup directory... already exists\n";
+	}
+
+	# backup directory does not exists
+	else{
+		mkdir ("backup");
+		print "Checking backup directory... backup directory created\n";
+	}
+
+	# Reads from file
+	use Cwd qw(cwd);
+	my $dir = cwd;
+	print "$dir\n";
+	open (my $fh, "+<", $filename) or die "Can't open $filename: $!";
+	while (<$fh>){
+		push @temp_storage, $_;
+		$line_count++;
+	}
+	# 10 lines or under
+	if ($line_count <= 10){
+		print "$filename has no more than 10 lines\n";
+		my $bckup = "./backup/$filename";
+		open (my $bu, ">", $bckup) or die "Can't write $bckup: $!";
+		foreach my $element (@temp_storage){
+			print $bu "$element";
+	}
+		close $bu;
+		print "Ok, old file backed up under backup directory\n";
+		
+		print $fh "Perl is cool!\n";
+		close $fh;
+		print "Wrote to file $filename\n";
+	}
+
+	# more than 10 lines
+	else{
+		while ($valid_input == 0) {
+			print "Enter 'c' to backup the first 10 lines. 'o' to overwrite without creatomg a backup\n";
+			my $usr_input = <STDIN>;
+			chomp $usr_input;
+			# Check if c
+			if ($usr_input eq "c"){
+				$valid_input = 1;
+				print"$usr_input is good\n";
+			}
+			
+			# Check if o
+			elsif ($usr_input eq "o"){
+				$valid_input = 1;
+				print"$usr_input is good\n";
+
+			}
+		
+		}
+	}
+}
+
+# file does not exists
+else {
+	open my $fh,">$filename" or die "Can't open $filename: $!";
+	print $fh "Perl is cool!\n";
+	close $fh;
+	print "Wrote to file $filename\n";
+}
+
