be6970860bbd4990b2cc6904c644da71d2c729ac
Kevin <kevinkuan4@gmail.com>
2018-03-22 02:00:15 -0400

    starting q4_p2

--- Kevin_Kuan_109645104/q4_p2.pl
+++ Kevin_Kuan_109645104/q4_p2.pl
@@ -2,6 +2,24 @@
 # CSE 337
 # Homework 2
 
+use strict;
+use warnings;
+
+
+sub find_in_file {
+	my $filename = $_[0];
+	open (my $fh, "<", $filename) or die "Can't open $filename: $!";
+	
+	
+}
+
+my @dir_list;
 print "What string do you want to search for?\n";
 my $usr_input = <STDIN>;
 
+opendir (my $dir, "./") or die "Can't open directory :$!";
+
+my @files = readdir $dir;
+closedir $dir;
+
+print @files[0];
